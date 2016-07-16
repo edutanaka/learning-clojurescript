ed1420e958addd0526e53c58c7c0c0ba680fe49a
edutanaka <edutanakabr@gmail.com>
2016-07-15 22:15:40 -0300

    first commit

---
+++ piggieback_project/project.clj
@@ -0,0 +1,11 @@
+(defproject piggieback_project "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.7.228"]
+                 [weasel "0.7.0" :exclusions [org.clojure/clojurescript]]]
+  :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.2.1"]
+                                  [org.clojure/tools.nrepl "0.2.10"]]
+                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}})
---
+++ piggieback_project/src/piggieback_project/core.cljs
@@ -0,0 +1,7 @@
+(ns piggieback-project.core
+  (:require [weasel.repl :as repl]))
+
+
+(when-not (repl/alive?)
+  (repl/connect "ws://localhost:9001"))
+
