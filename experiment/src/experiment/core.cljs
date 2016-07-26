2139d8e6810cdd942178228e8d6c49a15540ad35
edutanaka <edutanakabr@gmail.com>
2016-07-25 22:39:00 -0300

    experiment

---
+++ experiment/src/experiment/core.cljs
@@ -0,0 +1,21 @@
+(ns experiment.core
+  (:require [experiment.utils :refer [adder]]))
+
+(defn adder-multiplier
+  [x y z]
+  (* z (adder x y)))
+
+(enable-console-print!)
+
+(println "This text is printed from src/experiment/core.cljs. Go ahead and edit it and see reloading in action.")
+
+;; define your app data so that it doesn't get over-written on reload
+
+(defonce app-state (atom {:text "Hello world!"}))
+
+
+(defn on-js-reload []
+  ;; optionally touch your app-state to force rerendering depending on
+  ;; your application
+  ;; (swap! app-state update-in [:__figwheel_counter] inc)
+)
