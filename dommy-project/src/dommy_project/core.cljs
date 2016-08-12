41fa50a7dba2ed5052ff1c831e369b6af6e6e017
edutanaka <edutanakabr@gmail.com>
2016-08-11 21:01:05 -0300

    add dommy-project

---
+++ dommy-project/src/dommy_project/core.cljs
@@ -0,0 +1,21 @@
+(ns dommy-project.core
+  (:require [dommy.core :as dommy :refer-macros [sel sel1]] ))
+
+(defn set-borders!
+  []
+  (let [all-ps (sel [:#a-div :p])]
+    (->> all-ps
+         (map #(dommy/remove-class! % :changeme))
+         (map #(dommy/add-class! % :border))
+         (map #(dommy/set-text! % "I now have a border!")))
+    ))
+
+(defn add-btn!
+  []
+  (let [the-div (sel1 :#a-div)
+        a-btn (dommy/create-element "button")]
+    (dommy/set-text! a-btn "click me!")
+    (dommy/listen! a-btn :click
+                   (fn [e] (js/alert "you clicked me!")))
+    (-> the-div
+        (dommy/append! a-btn))))
